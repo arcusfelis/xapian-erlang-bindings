56b9b6d89847b64a474545c2d28e72831fe62563
Uvarov Michael <freeakk@gmail.com>
2012-06-23 22:31:10 +0400

    Add comments.

--- src/xapian_port.erl
+++ src/xapian_port.erl
@@ -13,6 +13,8 @@
         type :: atom()
 }).
 
+
+%% @doc Find and load dll or so file.
 load_driver() ->
     PrivDir = code:priv_dir(xapian),
     case erl_ddll:load_driver(PrivDir, ?DRIVER_NAME) of
@@ -24,22 +26,33 @@ load_driver() ->
                 "Error message: ~s~n"
                 "Error code: ~w~n",
             [?MODULE_STRING, ?LINE, ?DRIVER_NAME, PrivDir, Message, Error]),
-        erlang:exit(bad_lib)
+        erlang:exit(bad_port)
     end.
 
+
+%% @doc Open an instance (port).
 
 open(Type) ->
     #port_rec{type = Type, port = open_port(Type)}.
 
+%% @doc Close a port.
+%% If Port = port:
+%% * Release resources;
+%% * Close the port program;
+%% 
+%% If port = driver:
+%% * Release resources.
 
 close(#port_rec{port = Port}) ->
     erlang:port_close(Port).
 
 
+%% @doc Change the process's owner.
 connect(#port_rec{port = Port}, NewOwnerPid) ->
     erlang:port_connect(Port, NewOwnerPid).
 
 
+%% @doc Send a command.
 control(#port_rec{port = Port, type = driver}, Command, Data) ->
     erlang:port_control(Port, Command, Data);
 
@@ -59,10 +72,12 @@ control(#port_rec{port = Port, type = port}, Command, Data) ->
             
 
 
+%% Load erlang port driver (ddl).
 open_port(driver) ->
     load_driver(),
     erlang:open_port({spawn, ?DRIVER_NAME}, []);
 
+%% Run erlang port (exe).
 open_port(port) ->
     PrivDir = code:priv_dir(xapian),
     Exe = filename:join(PrivDir, ?PORT_NAME),
@@ -70,5 +85,6 @@ open_port(port) ->
     erlang:open_port({spawn_executable, Exe}, Opts).
 
 
+%% @doc Return true, if the passed port is active.
 is_port_alive(#port_rec{port = Port}) ->
     undefined =/= erlang:port_info(Port, id).
